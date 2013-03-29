93509ed16ea7556668d235a2749bc85561d8cd8f
Sam Cooper <sam@chgworks.com>
2013-03-29 01:30:50 -0700

    started on deploy

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+node.default.squash.root = "/srv/squash"
+node.default.squash.repo = "git://github.com/SquareSquash/web.git"
+node.default.squash.commit = "5f7a17a15e84cfe22c0f923107deed6181387daf" # circa April 2013
+node.default.squash.user = "deploy"
+node.default.squash.group = "deploy"
