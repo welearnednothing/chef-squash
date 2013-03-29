d806b2afa63798b198253eebaee74873a69e266f
Sam Cooper <sam@chgworks.com>
2013-03-29 13:07:28 -0700

    torquebox-server gem

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -2,7 +2,8 @@ node.default['rbenv']['rubies'] = [ "jruby-1.6.5", "1.9.3-p286" ]
 
 node.default['rbenv']['gems'] = {
   "jruby-1.6.5" => [
-    { "name" => "bundler" }
+    { "name" => "bundler" },
+    { "name" => "torquebox-server" }
   ]
 }
 
