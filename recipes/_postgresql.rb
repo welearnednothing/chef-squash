ff39784bb326c1d79bb10b15bf9581da926d383a
Sam Cooper <sam@chgworks.com>
2013-03-28 17:13:45 -0700

    postgresql override attrs

---
+++ recipes/_postgresql.rb
@@ -0,0 +1,12 @@
+node.override.postgresql.version = "9.2"
+node.override.postgresql.dir = "/var/lib/pgsql/9.2/data"
+node.override.postgresql.enable_pgdg_yum = true
+node.override.postgresql.client.packages = [ "postgresql92" ]
+node.override.postgresql.server.packages = [ "postgresql92-server", "postgresql92-devel" ]
+node.override.postgresql.service_name = "postgresql-9.2"
+node.override.postgresql.password.postgres = "changeme"
+node.override.postgresql.config_pgtune.db_type = "web"
+node.override.postgresql.config_pgtune.total_memory = "2097152kB"
+
+include_recipe "postgresql"
+
