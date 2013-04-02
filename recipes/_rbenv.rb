92d73319980dc0dce8f56e18f2361ad84d24cf82
Sam Cooper <sam@chgworks.com>
2013-04-02 11:45:19 -0700

    turn jruby back on

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -1,12 +1,13 @@
 node.default['rbenv']['rubies'] = [ "1.9.3-p286", "jruby-1.6.5" ]
+
 node.default['rbenv']['global'] = "1.9.3-p286"
 
-node.default['rbenv']['gems'] = {
-  "jruby-1.6.5" => [
-    { "name" => "bundler" },
-    { "name" => "trinidad" }
-  ]
-}
+# node.default['rbenv']['gems'] = {
+#   "jruby-1.6.5" => [
+#     { "name" => "bundler" },
+#     { "name" => "trinidad" }
+#   ]
+# }
 
 include_recipe "ruby_build"
 include_recipe "rbenv::system"
