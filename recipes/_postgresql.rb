843b7e51d91886b3fab2db05a1730e6e0675c3ae
Sam Cooper <sam@chgworks.com>
2013-04-03 14:47:50 -0700

    try a symlink to make pg build from source on scientific

--- recipes/_postgresql.rb
+++ recipes/_postgresql.rb
@@ -10,3 +10,31 @@ node.override.postgresql.config_pgtune.total_memory = "2097152kB"
 
 include_recipe "postgresql::server"
 
+# postgres needs to build from source b/c of issue w/ omnibus
+# http://lists.opscode.com/sympa/arc/chef/2012-10/msg00133.html
+link "/usr/bin/pg_config" do
+  to "/usr/pgsql-9.2/bin/pg_config"
+end
+
+include_recipe "postgresql::ruby"
+
+postgresql_connection_info = {
+  :host => "127.0.0.1",
+  :port => node['postgresql']['config']['port'],
+  :username => 'postgres',
+  :password => node['postgresql']['password']['postgres']
+}
+
+postgresql_database node['squash']['db'] do
+  connection postgresql_connection_info
+  action :create
+end
+
+postgresql_database_user 'foo_user' do
+  connection postgresql_connection_info
+  database_name node['squash']['db']
+  privileges [:all]
+  action :grant
+end
+
+
