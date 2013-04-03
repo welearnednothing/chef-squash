105b89b976e00286ca843b8a5ba052779b2e40a9
Sam Cooper <sam@chgworks.com>
2013-04-03 06:54:20 -0700

    use bundler and trinidad gems for MRI ruby too

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -2,6 +2,10 @@ node.default['rbenv']['rubies'] = [ "1.9.3-p286", "jruby-1.6.5" ]
 node.default['rbenv']['global'] = "1.9.3-p286"
 
 node.default['rbenv']['gems'] = {
+  "1.9.3-p286" => [
+    { "name" => "bundler" },
+    { "name" => "trinidad" }
+  ],  
   "jruby-1.6.5" => [
     { "name" => "bundler" },
     { "name" => "trinidad" }
