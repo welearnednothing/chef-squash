99a07e5c1015974588a44f864f8f55ffb546657e
Sam Cooper <sam@chgworks.com>
2013-04-04 23:36:08 -0700

    jruby 1.7.3 for 1.9.3 compatibility

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -1,15 +1,15 @@
-node.default['rbenv']['rubies'] = [ "1.9.3-p286", "jruby-1.6.5" ]
+node.default['rbenv']['rubies'] = [ "1.9.3-p286", "jruby-1.7.3" ]
 node.default['rbenv']['global'] = "1.9.3-p286"
 
 node.default['rbenv']['gems'] = {
   "1.9.3-p286" => [
     { "name" => "bundler" },
     { "name" => "trinidad" }
-  ],  
-  "jruby-1.6.5" => [
-    { "name" => "bundler" },
-    { "name" => "trinidad" }
-  ]
+  ] 
+  # "jruby-1.7.3" => [
+  #   { "name" => "bundler" },
+  #   { "name" => "trinidad" }
+  # ]
 }
 
 include_recipe "ruby_build"
