801c47c83539f9eb9861608afc270843ebdcb5d1
Sam Cooper <sam@chgworks.com>
2013-04-09 12:38:03 -0700

    configure squash nginx site to forward to 3000
    - add keys
    - setup squash.conf

--- recipes/_nginx.rb
+++ recipes/_nginx.rb
@@ -1,2 +1,35 @@
+node.default[:squash][:server_name] = "squash.blueboxgrid.com"
+node.default[:squash][:ssl_key_dir] = "/etc/nginx/ssl"
+node.default[:squash][:port] = "3000"
+
+directory node[:squash][:ssl_key_dir] do
+  owner "root"
+  group "root"
+  mode 00600
+  action :create
+  recursive true
+end
+
+# place keys
+cookbook_file "#{node[:squash][:ssl_key_dir]}/squash.blueboxgrid.com.key" do
+  mode "600"
+end
+
+cookbook_file "#{node[:squash][:ssl_key_dir]}/squash.blueboxgrid.com.crt" do
+  mode "755"
+end
+
+template "/etc/nginx/sites-available/squash.conf" do
+  source "squash_nginx_conf.erb"
+  variables  :server_name => node[:squash][:server_name],
+    :port => node[:squash][:port],
+    :ssl_certificate => "#{node[:squash][:ssl_key_dir]}/squash.blueboxgrid.com.crt",
+    :ssl_certificate_key => "#{node[:squash][:ssl_key_dir]}/squash.blueboxgrid.com.key"  
+  notifies :restart, "service[nginx]"
+end
+
+link "/etc/nginx/sites-enabled/squash.conf" do
+  to "/etc/nginx/sites-available/squash.conf"
+end
 
 include_recipe "nginx"
