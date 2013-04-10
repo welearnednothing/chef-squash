d82e12348072ecb5c4593643da64ebf6fd3614ba
Sam Cooper <sam@chgworks.com>
2013-04-10 14:18:59 -0700

    fixup for root and some halfass node attr reference changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,14 +27,14 @@ include_recipe "squash::_rbenv"
 include_recipe "squash::_postgresql"
 
 directory "#{node[:squash][:shared_dir]}/config/initializers" do
-  owner node.squash.user
-  group node.squash.group
+  owner node[:squash][:user]
+  group node[:squash][:group]
   recursive true
   notifies :run, "execute[chown_srv_squash]", :immediately
 end
 
 execute "chown_srv_squash" do
-  command "chown -R #{node.squash.user}:#{node.squash.group} #{node.squash.root}"
+  command "chown -R #{node[:squash][:user]}:#{node[:squash][:group]} #{node[:squash][:root_dir]}"
   action :nothing
 end
 
@@ -58,17 +58,17 @@ template "#{node[:squash][:shared_dir]}/config/initializers/secret_token.rb" do
   variables :secret => SecureRandom.hex
 end
 
-deploy_revision node.squash.root do
+deploy_revision node[:squash][:root_dir] do
   migrate true
   repository node.squash.repo
   revision node.squash.commit 
-  user node.squash.user
-  group node.squash.group
+  user node[:squash][:user]
+  group node[:squash][:group]
   symlinks ({ "secret_token.rb" => "config/initializers/secret_token.rb" })
   before_restart do
     execute "bundle" do
       command "bundle config build.pg --with-pg-config=/usr/pgsql-9.2/bin/pg_config && bundle install"
-      user node.squash.user
+      user node[:squash][:user]
       cwd node[:squash][:current_dir]
       only_if do
         File.exists? File.join(node[:squash][:current_dir], "Gemfile")
