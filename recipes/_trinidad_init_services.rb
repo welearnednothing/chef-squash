5ef486fc0bcc935ef94b9f7852543ff815224ad3
Sam Cooper <sam@chgworks.com>
2013-04-11 08:34:13 -0700

    redirect trininidad_init_services build command to /dev/null

--- recipes/_trinidad_init_services.rb
+++ recipes/_trinidad_init_services.rb
@@ -10,8 +10,9 @@ template init_config do
   variables :app_path => node[:squash][:root_dir]
 end
 
+# this command comes back as a failure from chef unless we file redirect to /dev/null
 execute "build_trinidad_init_service" do
-  command "RBENV_VERSION=#{node[:squash][:jruby][:version]} jruby -S trinidad_init_service --no-ask --defaults #{init_config}"
+  command "RBENV_VERSION=#{node[:squash][:jruby][:version]} jruby -S trinidad_init_service --no-ask --defaults #{init_config} > /dev/null 2>&1"
 end
 
 service "trinidad" do
