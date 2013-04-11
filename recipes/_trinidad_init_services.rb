b97e0b91405d2292a59110069d724bd217290754
Sam Cooper <sam@chgworks.com>
2013-04-11 12:19:16 -0700

    build jsvc from source using trinidad_init_services gem

--- recipes/_trinidad_init_services.rb
+++ recipes/_trinidad_init_services.rb
@@ -15,6 +15,11 @@ execute "build_trinidad_init_service" do
   command "RBENV_VERSION=#{node[:squash][:jruby][:version]} jruby -S trinidad_init_service --no-ask --defaults #{init_config} > /dev/null 2>&1"
 end
 
+# do this ourselves as it seems to be failing
+execute "compile_jsvc" do
+  command "RBENV_VERSION=jruby-1.7.3 jruby -e \"require 'trinidad_init_services'; _c = Trinidad::InitServices::Configuration.new; _c.send(:compile_jsvc, '/usr/local/src')\""
+end
+
 service "trinidad" do
   action [ :enable, :start ]
 end
