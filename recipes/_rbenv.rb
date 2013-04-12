1854b960412eb08f9a5b15237d7fa58214add77b
Sam Cooper <sam@chgworks.com>
2013-04-12 07:58:30 -0700

    give root some rbenv love too

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -10,7 +10,19 @@ node.default['rbenv']['user_installs'] = [
                      { "name" => "trinidad_init_services" }
                    ]
                  }
-  }
+  },
+  { 'user'    => 'root',
+    'rubies'  => [ '1.9.3-p286', node[:squash][:jruby][:version] ],
+    'global'  => '1.9.3-p286',
+    'gems'    => {
+                   '1.9.3-p286'    => [ { 'name'    => 'bundler' } ],
+                   node[:squash][:jruby][:version] => [
+                     { "name" => "bundler" },
+                     { "name" => "trinidad" },
+                     { "name" => "trinidad_init_services" }
+                   ]
+                 }
+  }                                          
 ]
 
 include_recipe "ruby_build"
