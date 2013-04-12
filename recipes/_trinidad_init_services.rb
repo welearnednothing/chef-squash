d18e2eb8d9bf20e93e589088ea837195c5103d13
Sam Cooper <sam@chgworks.com>
2013-04-11 23:50:59 -0700

    set deploy user in init script after the fact

--- recipes/_trinidad_init_services.rb
+++ recipes/_trinidad_init_services.rb
@@ -20,14 +20,20 @@ template init_config do
   variables :app_path => node[:squash][:current_dir]
 end
 
-# this command comes back as a failure from chef unless we file redirect to /dev/null
+# build config file for trinidad_init_service
 execute "build_trinidad_init_service" do
   command "RBENV_VERSION=#{node[:squash][:jruby][:version]} jruby -S trinidad_init_service --no-ask --defaults #{init_config} > /dev/null 2>&1"
 end
 
-# do this ourselves as it seems to be failing
+# jsvc get's used to build the init script
 execute "compile_jsvc" do
   command "RBENV_VERSION=jruby-1.7.3 jruby -e \"require 'trinidad_init_services'; _c = Trinidad::InitServices::Configuration.new; _c.send(:compile_jsvc, '/usr/local/src')\""
+end
+
+# the init script buiilder always fails when we try to set run_user to something
+# so set it to nothing and set the value in the init file
+execute "add_deploy_user_to_init" do
+  command "sed -i'' s/RUN_USER=\\\"\\\"/RUN_USER=\\\"deploy\\\"/ /etc/init.d/trinidad"
 end
 
 service "trinidad" do
