269fe39c6de1266bb4f99e707a0514789f71ddfa
Sam Cooper <sam@chgworks.com>
2013-04-12 02:54:22 -0700

    recipes/_rbenv.rb

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -1,7 +1,20 @@
 node.default['rbenv']['rubies'] = [ "1.9.3-p286", node[:squash][:jruby][:version] ]
 node.default['rbenv']['global'] = "1.9.3-p286"
 
-node.default['rbenv']['gems'] = {
+node['rvm']['user_rubies'] = [ "1.9.3-p286", node[:squash][:jruby][:version] ]
+
+# node.default['rbenv']['gems'] = {
+#   "1.9.3-p286" => [
+#     { "name" => "bundler" }
+#   ], 
+#    node[:squash][:jruby][:version] => [
+#     { "name" => "bundler" },
+#     { "name" => "trinidad" },
+#     { "name" => "trinidad_init_services" }
+#   ]
+# }
+
+node.default['rbenv']['user_gems'] = {
   "1.9.3-p286" => [
     { "name" => "bundler" }
   ], 
