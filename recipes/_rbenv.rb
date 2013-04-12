fc6c7ff7563328b863eafa80940867b4e050a9b5
Sam Cooper <sam@chgworks.com>
2013-04-12 08:07:05 -0700

    more rbenv fixup

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -1,3 +1,13 @@
+node.default['rbenv']['rubies'] = [ "1.9.3-p286", node[:squash][:jruby][:version] ]
+
+node.default['rbenv']['gems'] = {
+                   node[:squash][:jruby][:version] => [
+                     { "name" => "bundler" },
+                     { "name" => "trinidad" },
+                     { "name" => "trinidad_init_services" }
+                   ]  
+}
+
 node.default['rbenv']['user_installs'] = [
   { 'user'    => 'deploy',
     'rubies'  => [ '1.9.3-p286', node[:squash][:jruby][:version] ],
@@ -10,22 +20,11 @@ node.default['rbenv']['user_installs'] = [
                      { "name" => "trinidad_init_services" }
                    ]
                  }
-  },
-  { 'user'    => 'root',
-    'rubies'  => [ '1.9.3-p286', node[:squash][:jruby][:version] ],
-    'global'  => '1.9.3-p286',
-    'gems'    => {
-                   '1.9.3-p286'    => [ { 'name'    => 'bundler' } ],
-                   node[:squash][:jruby][:version] => [
-                     { "name" => "bundler" },
-                     { "name" => "trinidad" },
-                     { "name" => "trinidad_init_services" }
-                   ]
-                 }
-  }                                          
+  }               
 ]
 
 include_recipe "ruby_build"
+include_recipe "rbenv::system"
 include_recipe "rbenv::user"
 
 
