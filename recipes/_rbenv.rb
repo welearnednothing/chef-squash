513f6c3ab4be71b44ea219caf8c8c3641b3791b0
Sam Cooper <sam@chgworks.com>
2013-04-12 07:41:38 -0700

    rbenv fixup

--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -1,32 +1,20 @@
-# node.default['rbenv']['rubies'] = [ "1.9.3-p286", node[:squash][:jruby][:version] ]
-# node.default['rbenv']['global'] = "1.9.3-p286"
-
-node.default['rvm']['user_rubies'] = [ "1.9.3-p286", node[:squash][:jruby][:version] ]
-
-node['rvm']['user_installs'] = [
-                                { 'user'          => 'deploy',
-                                  'default_ruby'  => '1.9.3-p286',
-                                  'rubies'        => ['1.9.3-p286', node[:squash][:jruby][:version]]
-                                },
-                                { 'user'          => 'root',
-                                  'default_ruby'  => node[:squash][:jruby][:version],
-                                  'rubies'        => [ '1.9.3-p286', node[:squash][:jruby][:version] ]
-                                }                                
-                               ]
-
-node.default['rbenv']['user_gems'] = {
-  "1.9.3-p286" => [
-    { "name" => "bundler" }
-  ], 
-   node[:squash][:jruby][:version] => [
-    { "name" => "bundler" },
-    { "name" => "trinidad" },
-    { "name" => "trinidad_init_services" }
-  ]
-}
+node.default['rbenv']['user_installs'] = [
+  { 'user'    => 'deploy',
+    'rubies'  => [ '1.9.3-p286', node[:squash][:jruby][:version] ],
+    'global'  => '1.9.3-p286',
+    'gems'    => {
+                   '1.9.3-p286'    => [ { 'name'    => 'bundler' } ],
+                   node[:squash][:jruby][:version] => [
+                     { "name" => "bundler" },
+                     { "name" => "trinidad" },
+                     { "name" => "trinidad_init_services" }
+                   ]
+                 }
+  }
+]
 
 include_recipe "ruby_build"
-include_recipe "rbenv::user_install"
+include_recipe "rbenv::user"
 
 
 
