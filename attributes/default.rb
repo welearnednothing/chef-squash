75bf433e308f1c6d938b54fb51ae878747153005
Sam Cooper <scooper@bluebox.net>
2013-04-15 00:01:48 -0700

    updated attribues for community cookbook audience

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,10 @@
-node.default.squash.root_dir = "/srv/squash"
-node.default.squash.current_dir = "/srv/squash/current"
-node.default.squash.shared_dir = "/srv/squash/shared"
+# normally repo would be set to square's repo but I'm having this rake gem issue
+# https://github.com/SquareSquash/web/issues/71
+
+# node.default.squash.repo = "git://github.com/SquareSquash/web.git"
 node.default.squash.repo = "git://github.com/blueboxgroup/web.git"
-node.default.squash.revision = "bluebox"
+
+node.default.squash.revision = "master"
 node.default.squash.user = "deploy"
 node.default.squash.group = "deploy"
 node.default.squash.db.name = "squash"
@@ -10,9 +12,12 @@ node.default.squash.db.host = "localhost"
 node.default.squash.db.user = "postgres"
 node.default.squash.jruby.version = "jruby-1.7.3"
 node.default.squash.ruby.version = "1.9.3-p392"
-node.default[:squash][:server_name] = "squash.blueboxgrid.com"
-node.default[:squash][:ssl_key_dir] = "/etc/nginx/ssl"
-node.default[:squash][:port] = "3000"
+node.default.squash.server_name = "squash.example.com"
+node.default.squash.ssl_key_dir = "/etc/nginx/ssl"
+node.default.squash.port = "3000"
+node.default.squash.root_dir = "/srv/squash"
+node.default.squash.current_dir = "/srv/squash/current"
+node.default.squash.shared_dir = "/srv/squash/shared"
 
 
 
